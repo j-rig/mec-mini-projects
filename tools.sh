0db41c0e59c8b40243fbb9bff1f01d74f96a7ff8
jrighetti <josh.righetti@viasat.com>
2023-03-24 17:07:50 -0700

    finished 5.4.4

--- tools.sh
+++ tools.sh
@@ -2,3 +2,5 @@
 #docker build -t mec-mini-projects .
 
 #sudo docker run --network host --mount type=bind,source="$(pwd)"/mec-mini-projects,target=/app mec-mini-projects jupyter notebook --allow-root --ip 0.0.0.0
+
+#docker run -p 8888:8888 --mount type=bind,source="$(pwd)"/mec-mini-projects,target=/app mec-mini-projects jupyter notebook --allow-root --ip 0.0.0.0
